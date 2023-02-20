d91377eebeb7a210daa0d340649c18657ec09360
Aditya Nandwana <you@example.com>
2023-02-20 19:26:14 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+// console.log('Hi')
+
+const wrapper = document.getElementById("wrapper");
+
+
+let prev = 0;
+let next ;
+
+setInterval(() => {
+    let index = parseInt(Math.random() * combinations.length)
+    next = index
+
+    while (next===prev){
+        index = parseInt(Math.random() * combinations.length)
+        next=index
+    }
+
+        const combinaton = combinations[index]
+    console.log(combinaton)
+    wrapper.dataset.configuration = combinaton.configuration
+    wrapper.dataset.roundness = combinaton.roundness
+    prev = index
+}, 2000)
+
+
+const combinations = [
+    { configuration: 1, roundness: 1 },
+    { configuration: 1, roundness: 2 },
+    { configuration: 1, roundness: 4 },
+    { configuration: 2, roundness: 2 },
+    { configuration: 2, roundness: 3 },
+    // {configuration:3,roundness:3},
+]
+console.log(parseInt(Math.random() * combinations.length))
+// console.log()
